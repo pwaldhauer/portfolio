343256cf1a9caaf6ccf7794337e163d10d47d629
Philipp Waldhauer <ich@philippwaldhauer.de>
2015-07-02 22:38:27 +0200

    Google analytics

--- index.php
+++ index.php
@@ -144,5 +144,20 @@
         <?php endforeach ?>
     };
 </script>
+<script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-1739006-6']);
+    _gaq.push(['_trackPageview']);
+    _gaq.push(['_gat._anonymizeIp']);
+
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+</script>
 </body>
 </html>
