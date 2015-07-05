bf436b75fb1bd101a81af00246c6b19238f50af7
Philipp Waldhauer <ich@philippwaldhauer.de>
2015-07-05 20:03:11 +0200

    improved build system

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require('gulp');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var minifycss = require('gulp-minify-css');
+var copy = require('gulp-copy');
+var clean = require('gulp-clean');
+
 gulp.task('scripts', function () {
     return gulp.src(['js/**/*.js'])
         .pipe(gulp.dest('dist'));
 });
-
 
 gulp.task('css', function () {
     return gulp.src(['js/swiper/swiper.min.css', 'css/*.css'])
@@ -17,6 +19,17 @@ gulp.task('css', function () {
         .pipe(concat('all.css'))
         .pipe(minifycss({compatibility: 'ie8'}))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function () {
+    return gulp.src('build', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('build', ['css', 'scripts'], function () {
+
+    return gulp.src(['data.php', 'index.php', 'lib.php', 'dist/**/*', 'img/**/*'])
+        .pipe(copy('build'));
 });
 
 
